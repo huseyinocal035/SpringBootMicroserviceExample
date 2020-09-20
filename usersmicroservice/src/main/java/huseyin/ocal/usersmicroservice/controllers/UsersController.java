028b3566f7ca75fc1b912adbd5b4e05d4a986362
huseyinocal035 <huseyinocal035@gmail.com>
2020-09-20 16:17:13 +0300

    discovery service api gateway and user microservice added

---
+++ usersmicroservice/src/main/java/huseyin/ocal/usersmicroservice/controllers/UsersController.java
@@ -0,0 +1,15 @@
+package huseyin.ocal.usersmicroservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @GetMapping("/status")
+    public String status() {
+        return "working bro!";
+    }
+}
---
+++ usersmicroservice/src/test/java/huseyin/ocal/usersmicroservice/UsersmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package huseyin.ocal.usersmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
